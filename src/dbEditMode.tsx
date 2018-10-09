e4f2ee51f852c37f04318fd8915c3abf7d92d643
Gregoire <gregoire@31ten.network>
2018-10-09 14:27:55 +0800

    DbEditMode system

---
+++ src/dbEditMode.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+  class DbEditMode extends React.Component<{}, {}> {
+    public render() {
+      return (
+        <div> 
+            LocalStorage DB manual edition mode
+        </div>
+      );
+    } 
+  }
+
+  
+  
+  export default DbEditMode;
+  
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import DbEditMode from 'src/dbEditMode';
 
 ReactDOM.render(
   <App />,
+  // <DbEditMode />, // for modifying db manually
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
